0a7b731e834183ab4300185c337fe63d2cf00f82
chad hinds <chadrickj8@gmail.com>
2024-07-22 11:39:33 -0400

    complete getPasswordOptions

--- scripts.js
+++ scripts.js
@@ -90,7 +90,64 @@ let upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+    // Variable to store length of password from user input
+    var length = parseInt(
+        prompt('How many characters would you like your password to contain?')
+    );
 
+    // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+    if (isNaN(length)) {
+        alert('Password length must be provided as a number');
+        return;
+    }
+
+    // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+    if (length < 8 || length > 128) {
+        alert('Password length must be at least 8 characters and no more than 129 characters');
+        return;
+    }
+
+    // Variable to store boolean regarding the inclusion of special characters
+    var hasSpecialCharacters = confirm(
+        'Click OK to confirm including special characters.'
+    );
+
+    // Variable to store boolean regarding the inclusion of numeric characters
+    var hasNumericCharacters = confirm(
+        'Click OK to confirm including numeric characters.'
+    );
+
+    // Variable to store boolean regarding the inclusion of lowercase characters
+    var hasLowerCasedCharacters = confirm(
+        'Click OK to confirm including lowercase characters.'
+    );
+
+    // Variable to store boolean regarding the inclusion of uppercase characters
+    var hasUpperCasedCharacters = confirm(
+        'Click OK to confirm including uppercase characters.'
+    );
+
+    // Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
+    if (
+        hasSpecialCharacters === false &&
+        hasNumericCharacters === false &&
+        hasLowerCasedCharacters === false &&
+        hasUpperCasedCharacters === false
+    ) {
+        alert('Must select at least one character type');
+        return;
+    }
+
+    // Object to store user input
+    var passwordOptions = {
+        length: length,
+        hasSpecialCharacters: hasSpecialCharacters,
+        hasNumericCharacters: hasNumericCharacters,
+        hasLowerCasedCharacters: hasLowerCasedCharacters,
+        hasUpperCasedCharacters: hasUpperCasedCharacters,
+    };
+
+    return passwordOptions;
 }
 
 // Function for getting a random element from an array
